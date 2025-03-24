64239422c887ae167898c5f8f8160bde1cdfdb26
isaia <isaiahnoelsalazar474@gmail.com>
2025-03-24 12:15:35 +0800

    Dashboard

---
+++ app/src/main/java/com/salazarisaiahnoel/ulap/activities/Dashboard.java
@@ -0,0 +1,16 @@
+package com.salazarisaiahnoel.ulap.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.salazarisaiahnoel.ulap.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
